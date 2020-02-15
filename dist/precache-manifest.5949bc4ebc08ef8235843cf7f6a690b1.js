19e90fcff76f55fee1edf4e1157cb2c5ba50bbd0
xiaochuan <xiaochuan@iMac-xiaochuan.lan>
2020-02-16 00:58:56 +0800

    build file.

---
+++ dist/precache-manifest.5949bc4ebc08ef8235843cf7f6a690b1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017c0956362be92da0ef",
+    "url": "/css/app.2ed93455.css"
+  },
+  {
+    "revision": "41cf637f12f4c1fcb6e2",
+    "url": "/css/chunk-vendors.c412edf5.css"
+  },
+  {
+    "revision": "43824eefafff03674513ba2f7b1dcaa4",
+    "url": "/fonts/iconfont.43824eef.ttf"
+  },
+  {
+    "revision": "e388283f3d10db7800702138a18f331a",
+    "url": "/fonts/iconfont.e388283f.eot"
+  },
+  {
+    "revision": "11ff22c8023a3c9644cc8c78d1a234f5",
+    "url": "/img/377-341x164.11ff22c8.jpg"
+  },
+  {
+    "revision": "97cb23daa25e0b924e3547eda19dfbb9",
+    "url": "/img/iconfont.97cb23da.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "676d8101aa1402fde53d540fcd571e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017c0956362be92da0ef",
+    "url": "/js/app.c0b1c5e2.js"
+  },
+  {
+    "revision": "41cf637f12f4c1fcb6e2",
+    "url": "/js/chunk-vendors.90b9b794.js"
+  },
+  {
+    "revision": "ff2506e8e5609fd8122d570f85ea026a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
